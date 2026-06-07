35d3eb5ba75a380131f2556dfcadcf1d65e046b6
toxicdemonium-byte <toxicdemonium@gmail.com>
2026-06-07 16:29:13 +0530

    Add custom cursor following mouse movement

---
+++ Script.js
@@ -0,0 +1,15 @@
+const cursor =
+document.querySelector(".cursor");
+
+document.addEventListener(
+"mousemove",
+(e)=>{
+
+cursor.style.left =
+e.clientX + "px";
+
+cursor.style.top =
+e.clientY + "px";
+
+}
+);
