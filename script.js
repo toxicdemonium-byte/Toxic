51eb2392378cd1ca33d2d9801f489517f3592faf
toxicdemonium-byte <toxicdemonium@gmail.com>
2026-06-11 06:19:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -65,3 +65,71 @@ tool.style.display="none";
 });
 
 });
+
+/* PRELOADER */
+
+window.addEventListener("load",()=>{
+
+document.getElementById("preloader")
+.style.display="none";
+
+});
+
+/* TOP BUTTON */
+
+const topBtn =
+document.getElementById("topBtn");
+
+window.addEventListener("scroll",()=>{
+
+if(window.scrollY>400){
+topBtn.style.display="block";
+}else{
+topBtn.style.display="none";
+}
+
+});
+
+topBtn.onclick=()=>{
+
+window.scrollTo({
+top:0,
+behavior:"smooth"
+});
+
+};
+
+/* THEME SAVE */
+
+const savedTheme =
+localStorage.getItem("theme");
+
+if(savedTheme==="light"){
+document.body.classList.add("light");
+}
+
+themeBtn.addEventListener("click",()=>{
+
+document.body.classList.toggle("light");
+
+if(document.body.classList.contains("light")){
+localStorage.setItem("theme","light");
+}else{
+localStorage.setItem("theme","dark");
+}
+
+});
+
+/* MOBILE MENU */
+
+const menuBtn =
+document.querySelector(".menu-btn");
+
+const navLinks =
+document.getElementById("navLinks");
+
+menuBtn.addEventListener("click",()=>{
+
+navLinks.classList.toggle("active");
+
+});
