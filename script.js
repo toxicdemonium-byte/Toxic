3876e7d911af2d62823e94ae93172fdaadfc7956
toxicdemonium-byte <toxicdemonium@gmail.com>
2026-06-11 06:12:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -37,3 +37,31 @@ dropZone.style.borderColor="#3b82f6";
 dropZone.addEventListener("dragleave",()=>{
 dropZone.style.borderColor="rgba(255,255,255,.2)";
 });
+
+// Live Search
+
+const searchInput =
+document.querySelector(".search-box input");
+
+const tools =
+document.querySelectorAll(".tool-card");
+
+searchInput.addEventListener("keyup",()=>{
+
+const value =
+searchInput.value.toLowerCase();
+
+tools.forEach(tool=>{
+
+const text =
+tool.textContent.toLowerCase();
+
+if(text.includes(value)){
+tool.style.display="block";
+}else{
+tool.style.display="none";
+}
+
+});
+
+});
