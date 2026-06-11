e1db70a1f5688bfc08e737137e52ff9e41711a81
toxicdemonium-byte <toxicdemonium@gmail.com>
2026-06-11 06:01:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ function signin(){
     container.classList.remove("active");
 }
 
+
+
+
+
+
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove",(e)=>{
+cursor.style.left=e.clientX+"px";
+cursor.style.top=e.clientY+"px";
+});
+
+const themeBtn=document.getElementById("themeBtn");
+
+themeBtn.onclick=()=>{
+document.body.classList.toggle("light");
+};
